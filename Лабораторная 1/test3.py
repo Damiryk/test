3f341d501f62476dc33f9d27d0720de0f8f13b37
Damiryk <147313817+Damiryk@users.noreply.github.com>
2023-10-08 20:43:03 +0300

    Add files via upload

---
+++ Лабораторная 1/test3.py
@@ -0,0 +1,8 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# TODO Разделите участников на две команды
+
+list_players1 = list_players[0:3]
+list_players2 = list_players[3:6]
+print(list_players1)
+print(list_players2)
