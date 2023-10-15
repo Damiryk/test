07772a61593dc58aaffc8cd520693d06dc5e07e5
Damiryk <147313817+Damiryk@users.noreply.github.com>
2023-10-15 14:02:34 +0300

    Update test3.py

--- Лабораторная 1/test3.py
+++ Лабораторная 1/test3.py
@@ -1,8 +1,8 @@
 list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
 
 # TODO Разделите участников на две команды
-
-list_players1 = list_players[0:3]
-list_players2 = list_players[3:6]
+index_sredniy = len(list_players) // 2
+list_players1 = list_players[:index_sredniy]
+list_players2 = list_players[index_sredniy:]
 print(list_players1)
 print(list_players2)
