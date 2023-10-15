22d43295e228ba566f3765b3235a9c6fa5ebdbb4
Damiryk <147313817+Damiryk@users.noreply.github.com>
2023-10-15 14:00:43 +0300

    Update test1.py

--- Лабораторная 1/test1.py
+++ Лабораторная 1/test1.py
@@ -2,11 +2,9 @@ numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -
 
 # TODO заменить значение пропущенного элемента средним арифметическим
 index = numbers.index(None)
-n = numbers[0:index]
-c = numbers[index+1:]
-g = n + c
-f = sum(g)
-b = len(numbers)
-m = f/b
-numbers[4] = m
+stroka1 = numbers[:index]
+stroka2 = numbers[index+1:]
+sum_strok = stroka1+stroka2
+srednee_arifm = sum(sum_strok)/len(numbers)
+numbers[index] = srednee_arifm
 print("Измененный список:", numbers)
