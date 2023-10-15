8cab930df060ae05b47d243433d1cdfc4249e910
Damiryk <147313817+Damiryk@users.noreply.github.com>
2023-10-15 14:12:14 +0300

    Update test1.py

--- Лабораторная 1/test1.py
+++ Лабораторная 1/test1.py
@@ -4,7 +4,7 @@ numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -
 index = numbers.index(None)
 stroka1 = numbers[:index]
 stroka2 = numbers[index+1:]
-sum_strok = stroka1+stroka2
-srednee_arifm = sum(sum_strok)/len(numbers)
+sum_strok = stroka1 + stroka2
+srednee_arifm = sum(sum_strok) / len(numbers)
 numbers[index] = srednee_arifm
 print("Измененный список:", numbers)
