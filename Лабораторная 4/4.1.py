208ddbee1a726ac7215d07a73f0d6a7a1df805ba
Damiryk <147313817+Damiryk@users.noreply.github.com>
2023-11-15 17:48:10 +0300

    Update 4.1.py

--- Лабораторная 4/4.1.py
+++ Лабораторная 4/4.1.py
@@ -5,9 +5,8 @@ def task() -> float:
     """Сумма произведений двух значений в каждом словаре"""
     with open(r'.\input.json', 'r', encoding='utf-8') as file:
         file_ = json.load(file)
-    sum_values = 0
-    for i in file_:
-        sum_values += i['score'] * i['weight']
+    sum_values = sum([i["score"] * i["weight"] for i in file_])
+
     return round(sum_values, 3)
 
 
